4cd5e738265be88f5c92a4881de967ed431d41fe
Gilberto <gilbertocosta1103@gmail.com>
2023-05-09 12:06:17 -0300

    Implementação de screens para cadastro de prestador e cliente

--- main.py
+++ main.py
@@ -2,16 +2,44 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import Layout
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
 
 
-Window.clearColor = (5/255,205/255,55/255,1)
+class MainWin(Screen):
+    pass
 
-GUI = Builder.load_file("tela1.kv")
+
+class Cliente(Screen):
+    nome = ObjectProperty(None)
+    email = ObjectProperty(None)
+    tel = ObjectProperty(None)
+
+    def btn(self):
+        print("Nome: ", self.nome.text, "Email: ", self.email.text, "Tel: ", self.tel.text)
+
+
+class Prestador(Screen):
+    empresa = ObjectProperty(None)
+    cnpj = ObjectProperty(None)
+    servico = ObjectProperty(None)
+
+    def btn(self):
+        print("Nome: ", self.empresa.text, "Email: ", self.cnpj.text, "Tel: ", self.servico.text)
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file('iservice.kv')
+
 
 class Iservice(App):
     def build(self):
-        return GUI
+        return kv
 
 
-   
 Iservice().run()
